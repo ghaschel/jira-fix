3eda27edae2dd94f455e0e61a63ea4d00ca4d2e8
ghaschel <ghaschel@gmail.com>
2017-12-05 13:37:46 -0200

    Alteração do ícone de edição para que tenha o comportamento sticky. Correção de código e melhoria

--- jirafix.js
+++ jirafix.js
@@ -23,39 +23,50 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 // ==UserScript==
 // @name         JiraFix
-// @version      0.1
-// @description  Restritnge a inicialização do editor de texto ao clique do botão de editar.
+// @version      0.2
+// @description  Restritnge a inicializaÃ§Ã£o do editor de texto ao clique do botÃ£o de editar.
 // @author       Guilherme Haschel
-// @icon         https://bytebucket.org/ghaschel/jirafix/raw/b92a3ffaa1b135b3fe9fdb722fb42bd10899bc34/icon48.png
-// @icon64       https://bytebucket.org/ghaschel/jirafix/raw/b92a3ffaa1b135b3fe9fdb722fb42bd10899bc34/icon64.png
+// @icon         https://bytebucket.org/ghaschel/jirafix/raw/master/icon48.png
+// @icon64       https://bytebucket.org/ghaschel/jirafix/raw/master/icon64.png
 // @domain       sinaxkm.atlassian.net
 // @include      https://sinaxkm.atlassian.net/browse/*
 // @match        https://sinaxkm.atlassian.net/browse/*
-// @updateURL    https://bitbucket.org/ghaschel/jirafix/raw/501c638133037800bdae8a249df4c214d651fb32/jirafix.js
-// @downloadURL  https://bitbucket.org/ghaschel/jirafix/raw/501c638133037800bdae8a249df4c214d651fb32/jirafix.js
+// @updateURL    https://bitbucket.org/ghaschel/jirafix/raw/master/jirafix.js
+// @downloadURL  https://bitbucket.org/ghaschel/jirafix/raw/master/jirafix.js
 // @license      MIT
 // ==/UserScript==
 (function() {
     'use strict';
     $(document).ready(function($){
         window.MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-        var sheet;
         var target = document.querySelector("#description-val");
         var config = { attributes: true };
 
-        var composeButton = function() {
-            var $button = $('#description-val > .overlay-icon');
-            var $sticky = $button.children('.aui-icon');
-            console.log($sticky);
-            $button.removeClass('aui-iconfont-edit').removeClass('aui-icon-small').removeClass('aui-icon').addClass('pointer');
-            $button.append('<span class="aui-icon aui-icon-small aui-iconfont-edit fix"></span>');
-        };
+        var sheet = (function() {
+            var style = document.createElement("style");
+            style.appendChild(document.createTextNode(""));
+            document.head.appendChild(style);
+            return style.sheet;
+        })();
+
+        sheet.insertRule(".pointer { cursor: pointer; }", sheet.length);
+        sheet.insertRule(".fix { top: 3px; left: 3px; } ", sheet.length);
+        sheet.insertRule(".sticky-icon:before { transform: translateY(13px) translateX(-3px); left: 0px !important; top: -6px !important; float: right; position: sticky;} ", sheet.length);
 
         var listnr = function() {
             var $editor = $('#description-val > .user-content-block');
             $editor.on('click', function(event) {
-                event = event || window.event;
-                event.preventDefault();
+                console.log(event);
+                if (
+                    !(
+                        (event.target.localName === 'a' && event.target.className === 'external-link') ||
+                        (event.target.localName === 'a' && event.target.className === 'jira-issue-macro-key issue-link') ||
+                        (event.target.localName === 'img')
+                    )
+                ) {
+                    event = event || window.event;
+                    event.preventDefault();
+                }
             });
         };
 
@@ -66,13 +77,6 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
                 return true;
             }
         };
-
-        sheet = (function() {
-            var style = document.createElement("style");
-            style.appendChild(document.createTextNode(""));
-            document.head.appendChild(style);
-            return style.sheet;
-        })();
 
         var lookatmyhorse = new MutationObserver(function(mutations){
             if (typeof(mutations[2]) !== "undefined") {
@@ -86,9 +90,10 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         });
 
         lookatmyhorse.observe(target, config);
-        composeButton();
         listnr();
-        sheet.insertRule(".pointer { cursor: pointer; }");
-        sheet.insertRule(".fix { top: 3px; left: 3px; } ");
+    });
+
+    $(window).load(function() {
+        $('#description-val > .overlay-icon').addClass('sticky-icon');
     });
 })();
